568499737aea5d7ebbdf65b8fb0e702db773e176
SanathKumarMS <sanathkumar.ms@ymedialabs.com>
2019-08-09 11:42:15 +0530

    First screen of Assignment

---
+++ Assignment/Assignment/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Sanath Kumar M S on 08/08/19.
+//  Copyright © 2019 YML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    @IBAction func getName(_ sender: Any) {
+        let name = nameField!.text
+        print(name!)
+    }
+    
+    
+    @IBAction func programTransition(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
