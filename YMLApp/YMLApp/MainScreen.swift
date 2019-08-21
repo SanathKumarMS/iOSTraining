43e0ed9e7112ce383b8971b8a1f3f5e02b6ecd60
SanathKumarMS <sanathkumar.ms@ymedialabs.com>
2019-08-21 20:01:06 +0530

    Added extension, Removed Scroll Indicator, Added Tab Bar Controller

--- YMLApp/YMLApp/MainScreen.swift
+++ YMLApp/YMLApp/MainScreen.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
+class MainScreen: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -21,10 +21,6 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     var descriptions = ["We are a design and innovation agency, creating digital products and experiences that have a lasting impact.","All things insurance, all things banking, all in one app.","The ultimate power tool: A best-in-class digital experience for The Home Depot.","Payment giant goes mobile-by-design.","The world's first intelligent air purifier, & the app putting clean air in people's hands." ]
     var logoNames = ["","4logo", "2logo", "3logo", "1logo"]
     
-    var count = 0
-    
-    var firstCell: UICollectionViewCell!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,15 +31,14 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         configurePageControl()
     }
     
+    //Setup the UI
     func setupUI(){
         self.collectionView.isPagingEnabled = true
         getStartedButton.layer.borderWidth = 2
         getStartedButton.layer.borderColor = UIColor.black.cgColor
-        //getStartedButton.layer.cornerRadius = 10
-        //getStartedButton.titleEdgeInsets = UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10)
-        //getStartedButton.contentEdgeInsets = UIEdgeInsets(top: 5,left: 5,bottom: 5,right: 5)
     }
     
+    //Setup the page control
     func configurePageControl()
     {
         self.pageIndicator.numberOfPages = titles.count
@@ -53,6 +48,12 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         self.pageIndicator.currentPageIndicatorTintColor = .black
         self.view.addSubview(pageIndicator)
     }
+    
+    
+}
+
+
+extension MainScreen: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate{
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -66,24 +67,27 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? MainScreenCell
         
-        
-        cell?.imageView.image = UIImage(named: imageNames[indexPath.row])
-        cell?.titleLabel.text = titles[indexPath.row]
-        cell?.titleLabel.sizeToFit()
-        //cell?.titleLabel.font = UIFont(name: "sailecbold", size: 24.0)
-        cell?.logo.image = UIImage(named: logoNames[indexPath.row])
-        cell?.descriptionLabel.text = descriptions[indexPath.row]
-        //cell?.descriptionLabel.sizeToFit()
-//        if(count == 0){
-//            cell?.titleLabel.font = UIFont.systemFont(ofSize: 34)
-//            count = 1
-//        }
+        print(indexPath.row)
+        print("Item \(indexPath.item)")
+        if(indexPath.row == 10)
+        {
+            cell?.imageView.image = nil
+            cell?.logo.image = nil
+            cell?.titleLabel.text = titles[indexPath.row]
+            cell?.descriptionLabel.text = descriptions[indexPath.row]
+            cell?.titleLabel.font = UIFont.systemFont(ofSize: 18)
+            print("First cell")
+        }
+        else
+        {
+            cell?.imageView.image = UIImage(named: imageNames[indexPath.row])
+            cell?.titleLabel.text = titles[indexPath.row]
+            cell?.titleLabel.sizeToFit()
+            cell?.logo.image = UIImage(named: logoNames[indexPath.row])
+            cell?.descriptionLabel.text = descriptions[indexPath.row]
+        }
         
         return cell ?? MainScreenCell()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("Index \(indexPath.row)")
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -93,21 +97,11 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
         pageIndicator.currentPage = Int(pageNumber)
-        print(scrollView.contentOffset.x)
     }
-    
-    
-    
-				    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+
+
+
+
