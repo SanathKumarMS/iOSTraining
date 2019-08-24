a2bff669edaeced391486bbd5a8778b8e8552cf9
SanathKumarMS <sanathkumar.ms@ymedialabs.com>
2019-08-24 11:08:20 +0530

    Started Contact Tab

---
+++ YMLApp/YMLApp/ContactVC.swift
@@ -0,0 +1,30 @@
+//
+//  ContactVC.swift
+//  YMLApp
+//
+//  Created by Sanath Kumar M S on 24/08/19.
+//  Copyright © 2019 YML. All rights reserved.
+//
+
+import UIKit
+
+class ContactVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
