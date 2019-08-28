6c0a412b1b8cb62376a39b39da0a7b3eae5f5902
SanathKumarMS <sanathkumar.ms@ymedialabs.com>
2019-08-28 12:27:34 +0530

    Implementing Network Manager

--- YMLApp/YMLApp/View Model/CareerViewModel.swift
+++ YMLApp/YMLApp/View Model/CareerViewModel.swift
@@ -17,52 +17,79 @@ class CareerViewModel{
     let userKey = "5b87065d-b207-44fc-aa26-b9e1253720d6"
     let projectKey = "9a5070e8-cd53-46d4-ae0a-c25f3458c81c"
     let videoURL = "https://vimeo.com/293855154"
+//    var headers = ["Content-Type": "application/json", "JsonStub-User-Key" : userKey, "JsonStub-Project-Key": projectKey]
     
     typealias jsonHandler = (() -> Void)
     typealias videoHandler = ((URL) -> Void)
     
+//    func loadPositionsFromJson(_ compHandler: @escaping jsonHandler){
+//
+//        //
+//        guard let url = URL(string: jsonURL) else { return }
+//
+//        var request = URLRequest(url: url)
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.addValue(userKey, forHTTPHeaderField: "JsonStub-User-Key")
+//        request.addValue(projectKey, forHTTPHeaderField: "JsonStub-Project-Key")
+//        //In Network Manager
+//
+//        let session = URLSession.shared.dataTask(with: request, completionHandler: {
+//            //
+//            data,response,error in
+//
+//            if let error = error{
+//                print(error)
+//            }
+//            if let _ = response, let data = data{
+//
+//                do {
+//                    //print(String(data: data, encoding: .utf8) ?? "")
+//                    let root = try JSONDecoder().decode(Root.self, from: data)
+//                    //print(root)
+//                    let data = root.data
+//                    self.jsonItems = data
+//                    for item in data{
+//                        print(item.domain)
+//                        print(item.position)
+//                        print(item.location)
+//                    }
+//                    DispatchQueue.main.async {
+//                        compHandler()
+//                    }
+//                } catch {
+//                    print("Error : \(error)")
+//                }
+//            }
+//            //In VM (Here only)
+//        })
+//        session.resume()
+//    }
+    
     func loadPositionsFromJson(_ compHandler: @escaping jsonHandler){
         
-        //
-        guard let url = URL(string: jsonURL) else { return }
-        
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue(userKey, forHTTPHeaderField: "JsonStub-User-Key")
-        request.addValue(projectKey, forHTTPHeaderField: "JsonStub-Project-Key")
-        //In Network Manager
-        
-        let session = URLSession.shared.dataTask(with: request, completionHandler: {
-            //
-            data,response,error in
-            
-            if let error = error{
-                print(error)
-            }
-            if let _ = response, let data = data{
-                
-                do {
-                    //print(String(data: data, encoding: .utf8) ?? "")
-                    let root = try JSONDecoder().decode(Root.self, from: data)
-                    //print(root)
-                    let data = root.data
-                    self.jsonItems = data
-                    for item in data{
-                        print(item.domain)
-                        print(item.position)
-                        print(item.location)
-                    }
-                    DispatchQueue.main.async {
-                        compHandler()
-                    }
-                } catch {
-                    print("Error : \(error)")
+        let headers = ["Content-Type": "application/json", "JsonStub-User-Key" : userKey, "JsonStub-Project-Key": projectKey]
+        NetworkManager.shared.get(urlString: jsonURL, headers: headers, successHandler: {
+            [weak self] (data) in
+            do{
+                let root = try JSONDecoder().decode(Root.self, from: data)
+                print(root)
+                let data = root.data
+                self?.jsonItems = data
+                for item in data{
+                    print(item.domain)
+                    print(item.position)
+                    print(item.location)
+                }
+                DispatchQueue.main.async {
+                    compHandler()
                 }
             }
-            //In VM (Here only)
+            catch{
+                print("Error")
+            }
         })
-        session.resume()
     }
+
     
     func playVideo(_ completionHandler: @escaping videoHandler){
         guard let url = URL(string: videoURL) else{
--- YMLApp/YMLApp/View Model/NetworkManager.swift
+++ YMLApp/YMLApp/View Model/NetworkManager.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
-class NetworkManager{
+protocol HTTPMethods{
+    func get(urlString: String, headers: [String: String]?, successHandler: @escaping (Data) -> Void)
+    func post(urlString: String, headers: [String: String]?, succesHandler: @escaping () -> Void)
+}
+
+
+class NetworkManager: HTTPMethods{
     
     static let shared = NetworkManager()
     
@@ -19,12 +25,70 @@ class NetworkManager{
         print("In GET")
     }
     
-    func get(request: URLRequest, comp: @escaping (Data?, URLResponse?, Error?) -> Void){
-//        URLSession.shared.dataTask(with: request) {[weak self] (data, respnse, err) in
-//            guard let weaself = self else { return }
-//            comp(data, respnse, err)
-//        }
+    
+    func get(urlString: String, headers: [String : String]? = nil, successHandler: @escaping (Data) -> Void) {
+        guard let url = URL(string: urlString) else { return}
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        if let headers = headers{
+            for (key,value) in headers{
+                urlRequest.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        URLSession.shared.dataTask(with: urlRequest, completionHandler: { [weak self] (data, response, error) in
+            if let error = error{
+                print(error.localizedDescription)
+                return
+            }
+            if let response = response as? HTTPURLResponse{
+                if response.statusCode >= 200 && response.statusCode < 300{
+                    if let data = data{
+                        successHandler(data)
+                    }
+                    else{
+                        print("Invalid Data")
+                        return
+                    }
+                }
+                else{
+                    print("Invalid response")
+                    return
+                }
+            }
+        }).resume()
     }
+    
+    func post(urlString: String, headers: [String : String]? = nil, succesHandler: () -> Void) {
+        
+    }
+    
+//    func get(request: URLRequest, comp: @escaping (Data?, URLResponse?, Error?) -> Void){
+////        URLSession.shared.dataTask(with: request) {[weak self] (data, respnse, err) in
+////            guard let weaself = self else { return }
+////            comp(data, respnse, err)
+////        }
+//
+//        URLSession.shared.dataTask(with: request, completionHandler: comp)
+//        .resume()
+//    }
+//
+//    func get(request: URLRequest, successHandler: @escaping (Data) -> Void){
+//        let completionHandler: (Data?, URLResponse?, Error?) -> Void = {(data, response, error) in
+//            if let error = error{
+//                print(error.localizedDescription)
+//            }
+//            if let response = response as? HTTPURLResponse{
+//                if response.statusCode >= 200 && response.statusCode < 300{
+//                    guard let data = data else { return}
+//                    successHandler(data)
+//                }
+//                else{
+//                    print("Invalid response")
+//                }
+//            }
+//        }
+//        URLSession.shared.dataTask(with: request, completionHandler: completionHandler)
+//    }
 }
 
 
